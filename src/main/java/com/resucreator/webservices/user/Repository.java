dc50738dbafed86bc41fcb2ace5fefe9c2a09a57
tirthya_kamal_dasgupta <tirthya.dasgupta@alliedmedia.com>
2023-01-04 21:54:35 +0530

    Project skeleton generated with gradle User model constructed

---
+++ src/main/java/com/resucreator/webservices/user/Repository.java
@@ -0,0 +1,7 @@
+package com.resucreator.webservices.user;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface Repository extends MongoRepository<User, String> {
+    
+}
