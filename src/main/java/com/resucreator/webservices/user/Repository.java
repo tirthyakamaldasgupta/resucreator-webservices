b7ae797d9110c83a6396760919ea2b84bb5a1a67
tirthya_kamal_dasgupta <tirthya.dasgupta@alliedmedia.com>
2023-01-05 15:56:39 +0530

    Minor changes

--- src/main/java/com/resucreator/webservices/user/Repository.java
+++ src/main/java/com/resucreator/webservices/user/Repository.java
@@ -2,6 +2,4 @@ package com.resucreator.webservices.user;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface Repository extends MongoRepository<User, String> {
-    
-}
+public interface Repository extends MongoRepository<User, String> {}
