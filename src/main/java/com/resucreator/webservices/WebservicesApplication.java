4ec29d457bc56bc478f8ead26d85d54b3c0e9d9d
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2023-01-21 12:34:50 +0530

    Fixed cors issue to a level

--- src/main/java/com/resucreator/webservices/WebservicesApplication.java
+++ src/main/java/com/resucreator/webservices/WebservicesApplication.java
@@ -2,12 +2,25 @@ package com.resucreator.webservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WebservicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebservicesApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry corsRegistry) {
+				corsRegistry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+			}
+		};
 	}
 
 }
